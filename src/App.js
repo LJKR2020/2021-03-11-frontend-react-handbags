947f80e945937830b877c5c3680e9094ba27c414
L.J. Kraamer-Rietveld <loredana.rietveld@gmail.com>
2021-03-11 17:43:02 +0100

    button first version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import './App.css';
+import Button from "./components/Button";
+
+import bag1 from "./assets/bag_1.png"
+import bag2 from "./assets/bag_2.png"
+import bag3 from "./assets/bag_3.png"
+import bag4 from "./assets/bag_4.png"
 
 function App() {
   return (
-      <h1>Handbags & Purses</h1>
+      <>
+        <h1>Handbags & Purses</h1>
+        <nav>
+            <button type="button" onClick={() => console.log("To the collection")}>
+                to the collection
+            </button>
+            <button type="button" onClick={() => console.log("Shop all bags")}>
+                shop all bags
+            </button>
+            <button type="button" onClick={() => console.log("Pre-orders")}>
+                pre-orders
+            </button>
+        </nav>
+          <main>
+              <article>
+                  <span></span>
+                  <img src={"./assets/bag_1.png"} alt={"bag1"}/>
+              </article>
+          </main>
+      </>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button (text, log) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Button;
